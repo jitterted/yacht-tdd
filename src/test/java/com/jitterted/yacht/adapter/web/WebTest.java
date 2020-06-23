ac9020234244fbd028d9cbb19388a3a018887cb7
Ted M. Young <tedyoung@gmail.com>
2020-06-23 15:57:16 -0700

    Moved controller to src/main where it should have been
    Tagged Spring tests so we don't run them with the rest of the unit tests

--- src/test/java/com/jitterted/yacht/adapter/web/WebTest.java
+++ src/test/java/com/jitterted/yacht/adapter/web/WebTest.java
@@ -1,5 +1,6 @@
 package com.jitterted.yacht.adapter.web;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -13,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
+@Tag("spring")
 public class WebTest {
 
   @Autowired
