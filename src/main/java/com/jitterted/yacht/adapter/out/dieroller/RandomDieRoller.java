3ae32e019985480aaeab7407ff59a3992ff1bde8
Ted M. Young <tedyoung@gmail.com>
2022-11-22 15:09:20 -0800

    Converted Nullable from using if...then and a flag to use an Embedded Stub.

--- src/main/java/com/jitterted/yacht/adapter/out/dieroller/RandomDieRoller.java
+++ src/main/java/com/jitterted/yacht/adapter/out/dieroller/RandomDieRoller.java
@@ -6,21 +6,51 @@ import java.util.Random;
 
 public class RandomDieRoller implements DieRoller {
 
-    private final Random random = new Random();
+    private final RandomInt random;
 
-    private RandomDieRoller() {
+    private RandomDieRoller(RandomInt randomInt) {
+        this.random = randomInt;
     }
 
     public static RandomDieRoller create() {
-        return new RandomDieRoller();
+        return new RandomDieRoller(new RandomWrapper());
     }
 
     static RandomDieRoller createNull() {
-        return new RandomDieRoller();
+        return new RandomDieRoller(new RandomStub());
     }
 
     @Override
     public int roll() {
         return random.nextInt(6) + 1;
     }
+
+
+    // ---- Nullable stuff below
+
+
+    private interface RandomInt {
+        int nextInt(int bound);
+    }
+
+    private static class RandomWrapper implements RandomInt {
+        private final Random random;
+
+        public RandomWrapper() {
+            this.random = new Random();
+        }
+
+        @Override
+        public int nextInt(int bound) {
+            return this.random.nextInt(bound);
+        }
+    }
+
+    private static class RandomStub implements RandomInt {
+        @Override
+        public int nextInt(int bound) {
+            return 0;
+        }
+    }
+
 }
--- src/test/java/com/jitterted/yacht/adapter/out/dieroller/RandomDieRollerTest.java
+++ src/test/java/com/jitterted/yacht/adapter/out/dieroller/RandomDieRollerTest.java
@@ -1,6 +1,17 @@
 package com.jitterted.yacht.adapter.out.dieroller;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 class RandomDieRollerTest {
 
+    @Test
+    void nullRollerAlwaysReturnsOne() throws Exception {
+        RandomDieRoller roller = RandomDieRoller.createNull();
+
+        assertThat(roller.roll())
+                .isEqualTo(1);
+    }
 
 }
