1eb0f5229737b1006fef01bdc2718b9eaa58d6a0
Ted M. Young <tedyoung@gmail.com>
2022-11-29 16:54:09 -0800

    Testing dice rolling and rerolling via GameService.

--- src/test/java/com/jitterted/yacht/application/GameServiceDieRollerTest.java
+++ src/test/java/com/jitterted/yacht/application/GameServiceDieRollerTest.java
@@ -1,36 +1,48 @@
 package com.jitterted.yacht.application;
 
+import com.jitterted.yacht.adapter.out.dieroller.DieRoller;
+import com.jitterted.yacht.application.port.ScoreCategoryNotifier;
+import com.jitterted.yacht.domain.HandOfDice;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class GameServiceDieRollerTest {
-//
-//    @Test
-//    public void multipleRollsShouldUseDieRoller() throws Exception {
-//        DieRoller dieRoller = DieRoller.createNull(5, 1, 4, 2, 3);
-//        DiceRoller diceRoller = new DiceRoller(dieRoller);
-//
-//        assertThat(diceRoller.roll())
-//                .isEqualTo(HandOfDice.of(5, 1, 4, 2, 3));
-//    }
-//
-//    @Test
-//    public void reRollKeepingTwoDiceResultsInNewRollIncludingKeptDice() throws Exception {
-//        DieRoller dieRoller = DieRoller.createNull(1, 2, 5);
-//        DiceRoller diceRoller = new DiceRoller(dieRoller);
-//        List<Integer> keptDice = List.of(3, 3);
-//
-//        HandOfDice diceRoll = diceRoller.reRoll(keptDice);
-//
-//        assertThat(diceRoll)
-//                .isEqualTo(HandOfDice.of(3, 3, 1, 2, 5));
-//    }
-//
-//    @Test
-//    public void reRollKeepingFourDiceResultsInNewRollIncludingKeptDice() throws Exception {
-//        DiceRoller diceRoller = new DiceRoller(DieRoller.createNull(4));
-//        List<Integer> keptDice = List.of(1, 1, 2, 2);
-//
-//        HandOfDice diceRoll = diceRoller.reRoll(keptDice);
-//
-//        assertThat(diceRoll)
-//                .isEqualTo(HandOfDice.of(1, 1, 2, 2, 4));
-//    }
+
+    private static final ScoreCategoryNotifier NO_OP_SCORE_CATEGORY_NOTIFIER = (diceRoll, score, scoreCategory) -> {
+    };
+
+    @Test
+    void rollDiceRollsAllDice() {
+        GameService gameService = createGameServiceWithDieRollsOf(5, 1, 4, 2, 3);
+        gameService.start();
+
+        gameService.rollDice();
+
+        assertThat(gameService.lastRoll())
+                .isEqualTo(HandOfDice.of(5, 1, 4, 2, 3));
+    }
+
+    @Test
+    void reRollingDiceKeepsSpecifiedDice() {
+        GameService gameService = createGameServiceWithDieRollsOf(6, 6, 6, 6, 6, 2, 3);
+        gameService.start();
+        gameService.rollDice();
+
+        gameService.reRoll(List.of(6, 6, 6));
+
+        assertThat(gameService.lastRoll())
+                .isEqualTo(HandOfDice.of(6, 6, 6, 2, 3));
+    }
+
+    private static GameService createGameServiceWithDieRollsOf(Integer... dies) {
+        DieRoller dieRoller = DieRoller.createNull(dies);
+        return new GameService(
+                NO_OP_SCORE_CATEGORY_NOTIFIER,
+                new AverageScoreFetcherStub(),
+                dieRoller);
+    }
+
 }
