91c469fda7a0cd2e0fec327dce1e5ca9794b5782
Ted M. Young <tedyoung@gmail.com>
2023-01-17 15:05:24 -0800

    Converted to Records

--- src/main/java/com/jitterted/yacht/adapter/out/scorecategory/RollAssignedToCategory.java
+++ src/main/java/com/jitterted/yacht/adapter/out/scorecategory/RollAssignedToCategory.java
@@ -5,16 +5,7 @@ import com.jitterted.yacht.domain.ScoreCategory;
 
 import java.util.stream.Collectors;
 
-class RollAssignedToCategory {
-    private final String roll;
-    private final String score;
-    private final String category;
-
-    public RollAssignedToCategory(String roll, String score, String category) {
-        this.roll = roll;
-        this.score = score;
-        this.category = category;
-    }
+record RollAssignedToCategory(String roll, String score, String category) {
 
     static RollAssignedToCategory from(HandOfDice handOfDice,
                                        int score,
@@ -30,52 +21,4 @@ class RollAssignedToCategory {
         );
     }
 
-    public String getRoll() {
-        return roll;
-    }
-
-    public String getScore() {
-        return score;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        RollAssignedToCategory that = (RollAssignedToCategory) o;
-
-        if (!roll.equals(that.roll)) {
-            return false;
-        }
-        if (!score.equals(that.score)) {
-            return false;
-        }
-        return category.equals(that.category);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = roll.hashCode();
-        result = 31 * result + score.hashCode();
-        result = 31 * result + category.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "RollAssignedToCategory{" +
-                "roll='" + roll + '\'' +
-                ", score='" + score + '\'' +
-                ", category='" + category + '\'' +
-                '}';
-    }
 }
