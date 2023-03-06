ccc88f1d0581b50a01edc8c13a753c8e165e43f1
Ted M. Young <tedyoung@gmail.com>
2023-03-06 15:30:41 -0800

    Deleted Ted's JPA implementation for persisting Game

--- src/main/java/com/jitterted/yacht/adapter/out/jpa/GameJpaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.jitterted.yacht.adapter.out.jpa;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface GameJpaRepository extends CrudRepository<GameDbo, Long> {
-}
