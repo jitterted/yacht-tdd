d75cb57cfcceeca9999ff2aebd0006289f812178
Ted M. Young <tedyoung@gmail.com>
2023-03-20 12:25:21 -0700

    Fixed @SpringBootTest by adding test config

--- src/test/java/com/jitterted/yacht/YachtApplicationTests.java
+++ src/test/java/com/jitterted/yacht/YachtApplicationTests.java
@@ -1,16 +1,28 @@
 package com.jitterted.yacht;
 
-import org.junit.jupiter.api.Disabled;
+import com.jitterted.yacht.application.GameService;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootTest
 @Tag("spring")
 class YachtApplicationTests {
 
-    @Disabled
-//    @Test
+    @Test
     void contextLoads() {
+    }
+
+    @TestConfiguration
+    public static class Configuration {
+        @Bean
+        @Primary
+        GameService createGameService() {
+            return GameService.createNull();
+        }
     }
 
 }
