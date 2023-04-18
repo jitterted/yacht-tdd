eb3ca29535fe7d6546c33edb91c9899421f421e7
Ted M. Young <tedyoung@gmail.com>
2023-04-18 15:04:38 -0700

    Dropped the Hex Arch Test (slows things down). Added updated maven wrapper.

--- src/test/java/com/jitterted/yacht/YachtApplicationTests.java
+++ src/test/java/com/jitterted/yacht/YachtApplicationTests.java
@@ -1,12 +1,8 @@
 package com.jitterted.yacht;
 
-import com.jitterted.yacht.application.GameService;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 
 @SpringBootTest
 @Tag("spring")
@@ -14,15 +10,6 @@ class YachtApplicationTests {
 
     @Test
     void contextLoads() {
-    }
-
-    @TestConfiguration
-    public static class Configuration {
-        @Bean
-        @Primary
-        GameService createGameService() {
-            return GameService.createNull();
-        }
     }
 
 }
